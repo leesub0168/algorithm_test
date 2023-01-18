cb29e73a81e8f99f256cf8b08684bf80fb2a1008
leeso <leesub0168@gmail.com>
2023-01-18 22:53:48 +0900

    알고리즘 테스트 소스 커밋(바킹독 실전 알고리즘 큐)

--- src/com/test/alg/Algorithm0117.java
+++ src/com/test/alg/Algorithm0117.java
@@ -374,7 +374,130 @@ class Test0117 {
         }
         bw.flush();
         bw.close();
+    }
 
+
+    public void queue_10845_arr(BufferedReader br, BufferedWriter bw) throws IOException {
+        int[] dat = new int[100001];
+        Arrays.fill(dat,-1);
+        int head = 0, tail = 0;
+        int n = Integer.parseInt(br.readLine());
+
+        while(n > 0) {
+            n--;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String m = st.nextToken();
+            if(m.equals("push")) {
+                dat[tail++] = Integer.parseInt(st.nextToken());
+            }else if(m.equals("front")) {
+                if(tail-head == 0) {
+                    bw.write("-1\n");
+                }else {
+                    bw.write(dat[head]+"\n");
+                }
+            }else if(m.equals("back")) {
+                if(tail-head == 0) {
+                    bw.write("-1\n");
+                }else {
+                    bw.write(dat[tail-1]+"\n");
+                }
+            }else if(m.equals("size")) {
+                bw.write((tail-head) + "\n");
+            }else if(m.equals("empty")) {
+                if(tail-head == 0) {
+                    bw.write("1\n");
+                }else {
+                    bw.write("0\n");
+                }
+            }else { // pop
+                if(tail-head == 0) {
+                    bw.write("-1\n");
+                }else {
+                    bw.write(dat[head] + "\n");
+                    head++;
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+    public void queue_10845(BufferedReader br, BufferedWriter bw) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new ArrayDeque<>();
+        int memo = -1;
+
+        while(n > 0) {
+            n--;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String m = st.nextToken();
+            if(m.equals("push")) {
+                memo = Integer.parseInt(st.nextToken());
+                queue.offer(memo);
+            }else if(m.equals("front")) {
+                if (queue.isEmpty()) {
+                    bw.write("-1\n");
+                }else {
+                    bw.write(queue.peek()+"\n");
+                }
+            }else if(m.equals("back")) {
+                if(queue.isEmpty()) {
+                    bw.write("-1\n");
+                }else {
+                    bw.write(memo + "\n");
+                }
+            }else if(m.equals("size")) {
+                bw.write(queue.size()+"\n");
+            }else if(m.equals("empty")) {
+                if(queue.isEmpty()) {
+                    bw.write("1\n");
+                }else {
+                    bw.write("0\n");
+                }
+            }else { // pop
+                if(queue.isEmpty()) {
+                    bw.write("-1\n");
+                }else {
+                    bw.write(queue.poll()+"\n");
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public void queueCard_2164(BufferedReader br, BufferedWriter bw) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i=1;i<=n;i++) {
+            q.offer(i);
+        }
+        while (!q.isEmpty()) {
+            if(q.size() == 1) {
+                bw.write(q.peek()+"\n");break;
+            }else {
+                q.poll();
+                int x = q.poll();
+                q.offer(x);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+    public void queueCard_2164_answer(BufferedReader br, BufferedWriter bw) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i=1;i<=n;i++) {
+            q.offer(i);
+        }
+        while (q.size() > 1) {
+            q.poll();
+            int x = q.poll();
+            q.offer(x);
+        }
+        bw.write(q.peek()+"\n");
+        bw.flush();
+        bw.close();
     }
 }
 public class Algorithm0117 {
@@ -383,7 +506,6 @@ public class Algorithm0117 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Test0117 test0117 = new Test0117();
-        test0117.tower_2493_arr(br,bw);
-
+        test0117.queueCard_2164(br,bw);
     }
 }
---
+++ src/com/test/alg/QueueTest.java
@@ -0,0 +1,60 @@
+package com.test.alg;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class QueueSample {
+    final int MX = 1000005;
+    int[] dat = new int[MX];
+    int head = 0, tail = 0;
+
+    public void push(int x) {
+        dat[tail++] = x;
+    }
+    public void pop() {
+        head++;
+    }
+    public int front() {
+        return dat[head];
+    }
+    public int back() {
+        return dat[tail-1];
+    }
+    public void test() {
+        push(10);push(20);push(30);
+        System.out.println(front());
+        System.out.println(back());
+        pop();pop();
+        push(15);push(25);
+        System.out.println(front());
+        System.out.println(back());
+    }
+
+    public void queueTest() {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+        System.out.println(q.size());
+        if(q.isEmpty())  {
+            System.out.println("Q is Empty");
+        }else {
+            System.out.println("Q is not Empty");
+        }
+        q.poll();
+        System.out.println(q.peek());
+        System.out.println(q.peek());
+        q.offer(40);
+        q.poll();
+        System.out.println(q.peek());
+
+    }
+
+}
+public class QueueTest {
+    public static void main(String[] args) {
+        QueueSample queueSample = new QueueSample();
+        queueSample.test();
+    }
+}
