3378a61035ef5ff6be80d8685794eec7144e21aa
leeso <leesub0168@gmail.com>
2023-01-31 23:08:05 +0900

    알고리즘 테스트 소스 커밋(바킹독 실전 알고리즘 재귀 - 하노이의탑)

--- src/com/test/alg/Algorithm0130.java
+++ src/com/test/alg/Algorithm0130.java
@@ -3,7 +3,7 @@ package com.test.alg;
 import java.io.*;
 import java.util.StringTokenizer;
 
-class Test0130 {
+class Test0130 { // 재귀
     public void multiply_1629(BufferedReader br, BufferedWriter bw) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         long a = Integer.parseInt(st.nextToken());
@@ -21,6 +21,27 @@ class Test0130 {
         if(b%2 == 0) return val;
         return val * a % c;
     }
+
+    public void hanoi_tower(BufferedReader br, BufferedWriter bw) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(((int)Math.pow(2,n) -1)+"\n");
+
+        move(1,3, n, bw);
+
+        bw.flush();
+        bw.close();
+    }
+
+    public void move(int a, int b, int n, BufferedWriter bw) throws IOException{
+        if(n == 1) {
+            bw.write(a + " " + b + "\n");
+            return;
+        }
+        move(a, 6-a-b, n-1, bw);
+        bw.write(a + " " + b + "\n");
+        move(6-a-b, b, n-1, bw);
+    }
 }
 
 public class Algorithm0130 {
@@ -29,7 +50,7 @@ public class Algorithm0130 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Test0130 test0130 = new Test0130();
-        test0130.multiply_1629(br,bw);
+        test0130.hanoi_tower(br,bw);
 
     }
 }
