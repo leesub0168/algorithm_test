820af60d3d8df943a4ce577652e1cec9d66bf9f7
leeso <leesub0168@gmail.com>
2023-02-01 01:12:32 +0900

    알고리즘 테스트 소스 커밋(바킹독 실전 알고리즘 재귀 )

--- src/com/test/alg/Algorithm0130.java
+++ src/com/test/alg/Algorithm0130.java
@@ -41,6 +41,7 @@ class Test0130 { // 재귀
         bw.write(a + " " + b + "\n");
         move(6-a-b, b, n-1, bw);
     }
+
     public void z_1074(BufferedReader br, BufferedWriter bw) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
@@ -60,6 +61,33 @@ class Test0130 { // 재귀
         return 3 * half * half + func(r-half, c-half, n-1);
     }
 
+    int cnt;
+    public void recursive_17478(BufferedReader br, BufferedWriter bw) throws IOException {
+        cnt = Integer.parseInt(br.readLine());
+        bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+
+        recurs(0, bw);
+        bw.flush();
+        bw.close();
+    }
+    public void recurs(int n, BufferedWriter bw) throws IOException {
+        _bar("\"재귀함수가 뭔가요?\"\n", n,bw);
+        if(n == cnt) {
+            _bar("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n", n,bw);
+        }else {
+            _bar("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n",n,bw);
+            _bar("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n",n,bw);
+            _bar("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n",n,bw);
+            recurs(n+1,bw);
+        }
+        _bar("라고 답변하였지.\n",n,bw);
+    }
+    public void _bar(String str, int stk, BufferedWriter bw) throws IOException{
+        for(int i=0;i<stk;i++) {
+            bw.write("____");
+        }
+        bw.write(str);
+    }
 }
 
 public class Algorithm0130 {
@@ -68,6 +96,6 @@ public class Algorithm0130 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Test0130 test0130 = new Test0130();
-        test0130.z_1074(br,bw);
+        test0130.recursive_17478(br,bw);
     }
 }
