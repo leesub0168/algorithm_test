2451fcc541f85f3b22b5e7daeda2c9e04c28eca5
leeso <leesub0168@gmail.com>
2023-02-01 18:39:06 +0900

    프로그래머스 - KAKAO BLIND 개인정보수집 유효기간

--- src/com/test/alg/Algorithm0130.java
+++ src/com/test/alg/Algorithm0130.java
@@ -1,7 +1,10 @@
 package com.test.alg;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 class Test0130 { // 재귀
     public void multiply_1629(BufferedReader br, BufferedWriter bw) throws IOException {
@@ -61,9 +64,9 @@ class Test0130 { // 재귀
         return 3 * half * half + func(r-half, c-half, n-1);
     }
 
-    int cnt;
+    int cnt1;
     public void recursive_17478(BufferedReader br, BufferedWriter bw) throws IOException {
-        cnt = Integer.parseInt(br.readLine());
+        cnt1 = Integer.parseInt(br.readLine());
         bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
 
         recurs(0, bw);
@@ -72,7 +75,7 @@ class Test0130 { // 재귀
     }
     public void recurs(int n, BufferedWriter bw) throws IOException {
         _bar("\"재귀함수가 뭔가요?\"\n", n,bw);
-        if(n == cnt) {
+        if(n == cnt1) {
             _bar("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n", n,bw);
         }else {
             _bar("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n",n,bw);
@@ -88,6 +91,130 @@ class Test0130 { // 재귀
         }
         bw.write(str);
     }
+
+
+    int[][] arr = new int[2200][2200];
+    int[] res = new int[3];
+    public void paper_count_1780(BufferedReader br, BufferedWriter bw) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+//        arr = new int[n][n];
+        for(int i=0;i<n;i++) {
+            String s = br.readLine();
+            for(int j=0;j<n;j++) {
+                arr[i][j] = Character.getNumericValue(s.charAt(j));
+            }
+        }
+        recurs1(0,0,n);
+        for(int i=0;i<3;i++) {
+            bw.write(res[i]+"\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+    public void recurs1(int x, int y, int z) {
+        if(check(x,y,z)) {
+            res[arr[x][y] +1] += 1;
+            return;
+        }
+        int n = z/3;
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                recurs1(x+i*n, y+j*n,n);
+            }
+        }
+    }
+    public boolean check(int x, int y, int n) {
+        for(int i=x;i<x+n;i++) {
+            for(int j=y;j<y+n;j++) {
+                if(arr[x][y] != arr[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        int today_num = Integer.parseInt(today.replaceAll("\\.",""));
+        int[] answer = new int[privacies.length];
+        int[] terms_int = new int[100];
+        for(int i=0;i<terms.length;i++) {
+            terms_int[terms[i].charAt(0) - 'A'] = Integer.parseInt(terms[i].split(" ")[1]);
+        }
+        int idx = 0;
+        for(int i=0;i<privacies.length;i++) {
+            String s = privacies[i];
+            char c = s.charAt(s.length()-1);
+
+            int year = Integer.parseInt(s.substring(0,4));
+            int month = Integer.parseInt(s.substring(5,7));
+            int day = Integer.parseInt(s.substring(8,10));
+
+            int t = terms_int[c-'A'];
+
+            year = year + ((t) / 12);
+            month = month + (t % 12);
+            if(month > 12) {
+                year = year + ((month) / 12);
+                month = (month) % 12;
+            }
+
+            if(s.substring(8,10).equals("01")) {
+                day = 28;
+                if(month > 1) {
+                    month--;
+                }else {
+                    month = 12;
+                    year--;
+                }
+            }else {
+                day--;
+            }
+
+            int num = Integer.parseInt(year + "" + String.format("%02d", month) + "" + String.format("%02d", day));
+
+            if(today_num > num) {
+                answer[idx++] = i+1;
+            }
+
+        }
+
+        int[] newAns = new int[idx];
+        for(int i=0;i<idx;i++) {
+            newAns[i] = answer[i];
+        }
+
+        return newAns;
+    }
+
+
+
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+
+        return (year * 12 * 28) + (month * 28) + day;
+    }
+    public int[] solution_refact(String today, String[] terms, String[] privacies) {
+        int today_num = getDate(today);
+        List<Integer> list = new ArrayList<>();
+        int[] terms_int = new int[100];
+
+        for(int i=0;i<terms.length;i++) {
+            terms_int[terms[i].charAt(0) - 'A'] = Integer.parseInt(terms[i].split(" ")[1]);
+        }
+
+        for(int i=0;i<privacies.length;i++) {
+            String[] privacy = privacies[i].split(" ");
+            if(today_num >= (getDate(privacy[0]) + terms_int[privacy[1].charAt(0) - 'A'] * 28)) {
+                list.add(i+1);
+            }
+        }
+
+        return list.stream().mapToInt(integer -> integer).toArray();
+    }
 }
 
 public class Algorithm0130 {
@@ -96,6 +223,14 @@ public class Algorithm0130 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Test0130 test0130 = new Test0130();
-        test0130.recursive_17478(br,bw);
+//        test0130.paper_count_1780(br,bw);
+        String[] a1 = {"A 6", "B 13", "C 3"};
+        String[] a2 = {"2021.05.02 A", "2022.11.01 B", "2022.02.19 C", "2022.02.20 C"};
+
+        int[] arr = test0130.solution("2022.05.19",a1,a2);
+
+        for(int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]+" ");
+        }
     }
 }
