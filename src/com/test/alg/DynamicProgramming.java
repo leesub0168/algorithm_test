280e167faf314b51d3ebe39bfed25c4f67f474a2
leeso <leesub0168@gmail.com>
2023-02-12 23:34:20 +0900

    백준 - DP(1,2,3 더하기)

--- src/com/test/alg/DynamicProgramming.java
+++ src/com/test/alg/DynamicProgramming.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class DP {
-    int x;
-    int[] dim;
     public int make_one(BufferedReader br) throws IOException {
-        x = Integer.parseInt(br.readLine());
-        dim = new int[x+2];
+        int x = Integer.parseInt(br.readLine());
+        int[] dim = new int[x+2];
 
         for(int i=2;i<=x;i++) {
             dim[i] = dim[i-1]+1;
@@ -20,12 +18,28 @@ class DP {
         return dim[x];
     }
 
+    public void plus_numbers_9095(BufferedReader br) throws IOException {
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0;i<t;i++) {
+            int x = Integer.parseInt(br.readLine());
+            int[] dim = new int[12];
+            dim[1] = 1;
+            dim[2] = 2;
+            dim[3] = 4;
+
+            for(int j=4;j<=x;j++) {
+                dim[j] = dim[j - 1] + dim[j - 2] + dim[j - 3];
+            }
+            System.out.println(dim[x]);
+        }
+    }
+
 }
 public class DynamicProgramming {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         DP dp = new DP();
-        System.out.println(dp.make_one(br));
+        dp.plus_numbers_9095(br);
     }
 }
