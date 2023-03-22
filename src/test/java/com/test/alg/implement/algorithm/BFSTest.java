00b75c49bb305337995d3bd19ef3c2346f1eecba
leeso <leesub0168@gmail.com>
2023-03-22 14:02:43 +0900

    import 정리

--- src/test/java/com/test/alg/implement/algorithm/BFSTest.java
+++ src/test/java/com/test/alg/implement/algorithm/BFSTest.java
@@ -2,9 +2,7 @@ package com.test.alg.implement.algorithm;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
